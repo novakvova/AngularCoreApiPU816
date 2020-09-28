bb158bafa3fa2ff51cffdc7737e8553a92b768dc
PavlMais <pavlomajstruk@gmail.com>
2020-09-28 10:22:05 +0300

    add enities

---
+++ WebApi/FastFood.WebApi/Entities/DbRole.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace FastFood.WebApi.Entities
+{
+    public class DbRole : IdentityRole<long>
+    {
+        public virtual ICollection<DbUserRole> UserRoles { get; set; }
+    }
+}
--- WebApi/FastFood.WebApi/Startup.cs
+++ WebApi/FastFood.WebApi/Startup.cs
@@ -1,14 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using FastFood.WebApi.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace FastFood.WebApi
 {
@@ -24,7 +21,19 @@ namespace FastFood.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
+            // Add framework services.
+            services.AddDbContext<EfContext>(options =>
+                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+
+            //services.AddIdentity<DbUser, DbRole>()
+            //    .AddEntityFrameworkStores<EFContext>();
+            services.AddIdentity<DbUser, DbRole>(options => options.Stores.MaxLengthForKeys = 128)
+                .AddEntityFrameworkStores<EfContext>()
+                .AddDefaultTokenProviders();
+            
             services.AddControllers();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
